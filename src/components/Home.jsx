ba107bc88bc4a515e8dc0b577f6987d401285985
Nacho A <48703982+MateSeco@users.noreply.github.com>
2020-11-05 10:13:39 -0300

    render de home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavComponent from "./NavComponent";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import Tweet from "./Tweet"
 
 function Home() {
   const token = useSelector((state) => state.token);
+  const[resData, setResData] = useState({ownTweet:"", tweets:[]})
 
   useEffect(() => {
   axios
       .get(`http://localhost:8000/home`, {headers: {'Authorization':`Bearer ${token}`}},)
-      .then((res) => res.data)
+      .then((res) => setResData({ ownTweet:res.data.ownTweet, tweets:res.data.tweets}))
       .catch((err) => console.log("err",err));
   }, []);
   
-
   return (
     <div className="homeBody">
       <NavComponent />
@@ -41,11 +42,10 @@ function Home() {
               Tweet
             </button>
           </form>
-          {/* 
-        <% for(let j = 0; j < tweets.length; j++) { %>
-        <hr />
-        <%- include('./partials/tweet.ejs', {user:tweets[j].author, tweets:
-        tweets[j], ownTweet: false}) %> <% } %> */}
+          { resData.tweets.map(tweet => {
+          return (<Tweet tweet={tweet} ownTweet={setResData.ownTweet} />)
+        })
+        }
         </div>
       </div>
       {/*      <footer><%- include('./partials/scripts.ejs'); %></footer> */}
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+const moment= require("moment")
 
-function Tweet() {
+function Tweet({tweet, ownTweet}) {
+  const dateFormated = moment(tweet.date).format("DD/MM/YYYY - HH:mm:ss");
   return (
     <div className="container">
       <div className="media mt-4 mb-4">
         <img
-          src="user.image"
+          src={tweet.author.image}
           className="mr-3 rounded-circle profileImageTweet"
           alt="..."
         />
@@ -14,29 +16,28 @@ function Tweet() {
           <h5 className="mt-0">
             <span>
               <Link to={"/user.userName"} className="links">
-                user.fullName
+              {tweet.author.firstName} {tweet.author.lastName}
               </Link>
             </span>
-            <span className="text-muted">@user.userName</span>
+            <span className="text-muted">@{tweet.author.userName}</span>
           </h5>
 
-          <p className="tweetFont"> tweets.content</p>
+          <p className="tweetFont"> {tweet.content}</p>
           <div className="tweetInfo">
-            <span> moment(tweets.date).format("DD/MM/YYYY - HH:mm:ss") </span>
+            <span> {dateFormated}</span>
             <span>
               <Link to={"/like/tweets._id"}>
                 <i className="far fa-heart heart"></i>
               </Link>
-              tweets.likes.length{" "}
+              {tweet.likes.length}{" "}
             </span>
-            {/* if (ownTweet) {  */}
+            {(ownTweet) && 
             <span>
               <Link to={"/user.userName/delete/tweets._id"} className="delete">
                 <i className="far fa-trash-alt ml-2"></i>
               </Link>
             </span>
-
-            {/*   <% } %> */}
+           } 
           </div>
         </div>
       </div>
