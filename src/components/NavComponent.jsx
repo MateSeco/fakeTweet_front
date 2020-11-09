61388d3c7235219782b46f2412a48a7e91f75f17
Nacho A <48703982+MateSeco@users.noreply.github.com>
2020-11-09 17:31:25 -0300

    ver nombre usuario en nav

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -9,6 +9,7 @@ import userActions from "../redux/Actions/userActions";
 function NavComponent() {
   const dispatch = useDispatch();
   const username = useSelector((state) => state.user.userName);
+
   return (
     <div>
       <Navbar bg="primary" variant="dark" className="twitter">
@@ -18,29 +19,30 @@ function NavComponent() {
               <span className="fakeTwitter">fake</span> Twitter
             </h1>
           </Navbar.Brand>
-
-          <Nav className="ml-auto accountStyle">
-            <NavDropdown
-              title="Account"
-              id="collasible-nav-dropdown"
-              className="accountStyle"
-            >
-              <NavDropdown.Item as={Link} to={`/${username}`}>
-                Profile
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to={`/${username}/settings`}>
-                Settings
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-                onClick={() => dispatch(userActions.loggedOut())}
-                as={Link}
-                to={`/`}
+          {username && (
+            <Nav className="ml-auto accountStyle">
+              <NavDropdown
+                title={`${username}`}
+                id="collasible-nav-dropdown"
+                className="accountStyle"
               >
-                Logout
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+                <NavDropdown.Item as={Link} to={`/${username}`}>
+                  Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={`/${username}/settings`}>
+                  Settings
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item
+                  onClick={() => dispatch(userActions.loggedOut())}
+                  as={Link}
+                  to={`/`}
+                >
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          )}
         </div>
       </Navbar>
     </div>
