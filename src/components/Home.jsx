a7c60d96ca73168e339cb28ad186ea19cc67cd1b
Nacho A <48703982+MateSeco@users.noreply.github.com>
2020-12-02 11:05:16 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,8 +14,8 @@ function Home() {
   useEffect(() => {
     dispatch(actions.saveTweets([]))
     axios
-      .get(`${process.env.REACT_APP_URL}/tweets`,{params: {limit: 10}}, {
-        headers: { Authorization: `Bearer ${token}` },
+      .get(`${process.env.REACT_APP_URL}/tweets`, {
+        headers: { Authorization: `Bearer ${token}` }, {params: {limit: 10}},
       })
       .then((res) => dispatch(actions.saveTweets(res.data.tweets)))
       .catch((err) => console.log("err", err));
